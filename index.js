4035fa00d030daa96df2e1b98a0dcadeaee2e150
jpitman1010 <jpitman1010@gmail.com>
2021-06-01 15:51:10 -0700

    types of functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Functions
 
+//performing a task (to display something on the console)
 function greet(name, lastName) {
     //name is a parameter only accessable by this function
     console.log('Hello ' + name + ' ' + lastName);
@@ -7,3 +8,21 @@ function greet(name, lastName) {
 
 //the arguement is the actual value supplying the parameter in the function above.
 greet('Julie', 'Pitman');
+
+
+//a fcn that calculates a value (must use return within fcn here)
+
+function square(number) {
+    return number * number
+}
+
+//declaring the function through a variable (variable declaration)
+let number = square(2);
+console.log(number)
+
+//console.log() is actually also a function call, 
+//passing an argument or another function
+//a function is a statement that either performs a task
+//or calculates and returns a value
+
+
