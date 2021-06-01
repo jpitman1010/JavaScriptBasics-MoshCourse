1f9337c6391561a3a9692e3000ab73d75e30955f
jpitman1010 <jpitman1010@gmail.com>
2021-06-01 16:04:00 -0700

    assignment operators

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-//Arithmetic Operators
+//Assignment Operators 
 
 let x = 10;
-let y = 3;
 
+// x++;
+//equivelent to x = x + 1
 
-// console.log(x + y);
-// console.log(x - y);
-// console.log(x * y);
-// console.log(x / y);
-// console.log(x % y); //remainder of division
-// console.log(x ** y);
-//an expression is something that produces a value, like x+y
+//but if you wanted to do increment by 5, this above statement won't work
+//instead you can use x += 5;
 
-//increment decorators indicated by  ++;
-// console.log(++x); //this displays 11
-//by putting ++ before the x, it will increment x up by 1 then display that result
-// console.log(x++);  //this displays 10
-//by putting ++ after the x, it will display the value of x the first console log, then
-//when console logged again it will increment x up by 1
-// console.log(x) //this displays 11 since it is called after the previous x++ console.log
+x += 5;
 
-//decrement decorators indicated by  --;
-// console.log(--x); //this displays 9
-//by putting ++ before the x, it will increment x up by 1 then display that result
-console.log(x--);  //this displays 10
-//by putting ++ after the x, it will display the value of x the first console log, then
-//when console logged again it will decrement x down by 1
-console.log(x) //this displays 9 since it is called after the previous x-- console.log
+//same goes for other operator types.
