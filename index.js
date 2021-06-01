761a6d307746b2a86d052df5d40ea640462b64f7
jpitman1010 <jpitman1010@gmail.com>
2021-06-01 16:01:25 -0700

    arithmetic operators

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-//Functions
+//Arithmetic Operators
 
-//performing a task (to display something on the console)
-function greet(name, lastName) {
-    //name is a parameter only accessable by this function
-    console.log('Hello ' + name + ' ' + lastName);
-}
-
-//the arguement is the actual value supplying the parameter in the function above.
-greet('Julie', 'Pitman');
+let x = 10;
+let y = 3;
 
 
-//a fcn that calculates a value (must use return within fcn here)
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y); //remainder of division
+// console.log(x ** y);
+//an expression is something that produces a value, like x+y
 
-function square(number) {
-    return number * number
-}
+//increment decorators indicated by  ++;
+// console.log(++x); //this displays 11
+//by putting ++ before the x, it will increment x up by 1 then display that result
+// console.log(x++);  //this displays 10
+//by putting ++ after the x, it will display the value of x the first console log, then
+//when console logged again it will increment x up by 1
+// console.log(x) //this displays 11 since it is called after the previous x++ console.log
 
-//declaring the function through a variable (variable declaration)
-let number = square(2);
-console.log(number)
-
-//console.log() is actually also a function call, 
-//passing an argument or another function
-//a function is a statement that either performs a task
-//or calculates and returns a value
-
-
+//decrement decorators indicated by  --;
+// console.log(--x); //this displays 9
+//by putting ++ before the x, it will increment x up by 1 then display that result
+console.log(x--);  //this displays 10
+//by putting ++ after the x, it will display the value of x the first console log, then
+//when console logged again it will decrement x down by 1
+console.log(x) //this displays 9 since it is called after the previous x-- console.log
