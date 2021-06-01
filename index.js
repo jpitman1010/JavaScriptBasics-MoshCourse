8def7bd7d7fa9fac41b1d0ef0dcff0696c19352c
jpitman1010 <jpitman1010@gmail.com>
2021-06-01 15:35:19 -0700

    javascript objects

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-let name = 'Julie';  //string literal
-let age = 30; //number literal
-let isApproved = true; //boolean literal
-let firstName = undefined; //undefined literal
-let selectedColor = null; //null literal - 
-//use if user doesn't have favorite color, it will
-//return null, but if they should choose a fav color
-//it can be changed to the color they choose.
 
+let person = {
+    name: 'Julie',
+    age: 30
+}; //using curly braces allows for 
+//an object literal, you can then use key value pairs 
+//to input attributes to that object.
+
+console.log('person object literal = ', person);
+
+//dot notation -this is cleaner coding and easier to use per Mosh
+person.name = 'John';
+
+console.log('person dot notation =', person.name);
+
+//bracket Notation
+person['age'] = '22';
+
+console.log('person bracket notation for age = ', person.age);
+
+
+let selection = 'name';
+
+//target property is to use selection
+person[selection] = 'Mary';
+
+console.log('target property of selection =', person.name);
