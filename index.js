143e748effcb4dfd70c9c9001b1b15424432b401
jpitman1010 <jpitman1010@gmail.com>
2021-06-01 11:12:42 -0700

    primitive/value types

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const interestRate = 0.3;
-interestRate = 1;
-console.log(interestRate);
+let name = 'Julie';  //string literal
+let age = 30; //number literal
+let isApproved = true; //boolean literal
+let firstName = undefined; //undefined literal
+let selectedColor = null; //null literal - 
+//use if user doesn't have favorite color, it will
+//return null, but if they should choose a fav color
+//it can be changed to the color they choose.
+
