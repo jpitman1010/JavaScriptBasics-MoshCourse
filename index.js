90d3002b0752bde5ac726c29c14901038b734af9
jpitman1010 <jpitman1010@gmail.com>
2021-06-01 16:18:55 -0700

    Ternary (conditional) operator

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-//equality operators
+//Ternary (conditional) Operator
 
-// === is strict equality operator
-//it ensures that both sides of the operator are exactly the same type and value
-console.log(1 === 1);  //true
-console.log('1' === 1); //false because the type of one operand 
-//is a string and the other a number
+//if a customer has more than 100 points, 
+//they are a 'gold customer, otherwise,
+//they are a 'silver' customer.
 
-//  == is a loose equality operator - doesn't care about types matching, it will convert
-//and only check to see that values are equal
-console.log(1 === 1); //true
-console.log('1' == 1); //true -if the operand on the left side is a string, it 
-//automatically converts the value on the right side to the same type.  so it is 
-//in the end comparing '1' to '1'
-console.log(true == 1);  //true - because it automatically turns the second operand/value 
-//to the same type as the left operand.
+let points = 110;
+let type = points > 100 ? 'gold' : 'silver';
 
-//should use strict most of the time because it is more precise and accurate.
+//by adding the ? at the end of the condition statement,
+//it will take the true false answer that is returned (is points greater than 100)
+//and then by putting 'gold' : 'silver' it is stating that if  true  is returned
+//then type === 'gold, if false type === 'silver
+
+console.log(type); //resul;ts in gold
+
+//if you change the points to = 90, then you would get output of silver from console.log(type)
+
+
