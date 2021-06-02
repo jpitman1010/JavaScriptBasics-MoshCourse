bea29c37c2e0d901ea50fcb63d8e71375fdd0034
jpitman1010 <jpitman1010@gmail.com>
2021-06-02 14:05:53 -0700

    exercise write a function returning max num

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-//break and continue
+// Exercise: Write a fcn that takes 2 numbers and returns the max of the two.
 
-let i = 0;
+function maxNum(num1, num2) {
+    // if (num1 > num2) return ( num1, console.log(num1));
+    // return (num2, console.log(num2));
+    //no need to include else when the only one if statement because if 
+    //that condition is false it will return the other
 
-// while (i <= 10) {
-//     if (i === 5) break;
-//     console.log(i);
-//     i++;
-// }
-
-while (i <= 10) {
-    if (i % 2 === 0) {
-        i++;
-        continue;
-        //considered an ugly way to code, it causes the loop to 
-        //jump into the next iteration;
-    }
-    console.log(i);
-    i++;
+    //this is another option of how to write the code above:
+    return (num1 > num2) ? num1 : num2;
 }
 
-//this outputs 1,3,5,7,9
-//whereas if the if statement stopped at if(i % 2) {statement}; then it would output even numbers
+maxNum(33,22);
+
+let number = maxNum(3,3);
+console.log(number);
+// let number = maxNum(3,1);
+// console.log(number);
+// let number = maxNum(1,3);
+// console.log(number);
+
+//always test functions with all diff. possible scenarios
