20ba4c5ee28c53171dc765c3c3b47c7a515eec5b
jpitman1010 <jpitman1010@gmail.com>
2021-06-01 18:08:54 -0700

    Bitwise Operators

--- index.js
+++ index.js
@@ -1,42 +1,35 @@
-//Logical Operators with Non-boolean values
+//Bitwise Operators
+// Doesn't have as much practical use in most of programming
+//decimal to binary - works on the individual bits of a number
+// 1 = 00000001
+// 2 = 00000010
+// 3 = 00000011
+// R = 00000000
 
-//  false || true      will return true
-// false || 'Mosh'     will return 'Mosh'
-// false || 1          will return 1
+console.log(1 | 2); // bitwise OR -returns 3 because it is combining 1 and 2
+console.log(1 & 2); // bitwise AND - returns 0
 
+// Read, Write, Execute
+    // 0000100  -Read
+    // 0000110 - Read and Write permissions
+    // 0000001 - Read, write and execute permissions
 
-//the result of a logical expression is not necessarily true or false.  
-//if the types do not match, it evaluates it as "Truthy" or "Falsy"
+const readPermission = 4;
+const writePermission = 2;
+const executePermission = 1;
 
-//Falsy (false) values:
-    // undefined
-    // null
-    // 0 
-    // false 
-    // ''
-    // NaN (not a number)
+let myPermission = 0;
+myPermission = myPermission | readPermission | writePermission;
+console.log(myPermission);
 
-//anything ont Falsy is Truthy and return whatever the other operand is, 
-//since the first operand is false, it continues to look on other side of ||
-//and finds the 'Mosh' or 1 to be truthy.  
-    
+let message = (myPermission & readPermission) ? 'yes': 'no';
+//if this condition evaluates to true (myPermission = readPermission) it will return 'yes'
+console.log(message);  //returns yes because myPermission does have readPermission
 
-//false || 1 || 2
-    //when we have more than one ||, if JavaScript finds the first Truthy part of the 
-    //equation, it stops.  So output for : false || 1 || 2 would be 1.   
-    //This is called short-circuting
-
-
-// let userColor = 'red';
-// let defaultColor = 'blue';
-// let currentColor = userColor || defaultColor;
-// //this last one states that if we have a value for userColor we will use that, if not
-// //we will use the default color.
-// console.log(currentColor); //returns 'red'
+//but if you remove the readPermission from myPermission like:
+//myPermission = myPermission | writePermission;
+// you will get 'no' because the statement is no longer true.
 
 
-let userColor = undefined;
-let defaultColor = 'blue';
-let currentColor = userColor || defaultColor;
 
-console.log(currentColor); //returns 'blue'
+
