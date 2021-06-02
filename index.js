d0b94da331361af409ae2808474616332faf58ef
jpitman1010 <jpitman1010@gmail.com>
2021-06-02 13:26:07 -0700

    do...while loops

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-//While
+//Do ... While
 
 //must declair the variable outside of the loop 
 //(for reasons of scope, to be learned later)
 
-let i =0;
+// let i =0;
 
-// while (condition) {
-//     statement
+// // while (condition) {
+// //     statement
+// // }
+
+// while (i <= 5) {
+//     if (i % 2 !== 0) console.log(i);
+//     i++;
 // }
 
-while (i <= 5) {
+
+
+//do while 
+
+// let i = 0;
+// do {
+//     if (i % 2 !== 0) console.log(i);
+//     i++;
+// } while (i <= 5);
+
+//do while loops are always evaluated at least 1x, even if condition evaluates to false
+
+let i = 9;
+do {
     if (i % 2 !== 0) console.log(i);
     i++;
-}
+} while (i <= 5);
+
+//will console log 9 in this expression since it always must run at least 1x
+//practically speaking will likely use a regular while loop
