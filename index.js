0b80122abed8a1e305719ea0b760f70caef58358
jpitman1010 <jpitman1010@gmail.com>
2021-06-02 14:33:43 -0700

    FizzBuzz exercise

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-//exercise write a fcn isLandscape(width, height) {
-//     return true if width greater than height;
-//     else return false
+// write a function fizzBuzz(input){
+//     return a string
+// }
+//if number is divisible by 3, we will get output of Fizz
+//if number is divisible by 5, we will get oupput of Buzz
+//if number is divisible by both 3 and 5, we get FizzBuzz output
+//else: return same number being passed into input
+//if passing a non Number, should return a message, "Not a number"
+
+
+//my answer
+// console.log(fizzBuzz(3));
+// console.log(fizzBuzz(15));
+// console.log(fizzBuzz(5));
+// console.log(fizzBuzz(22));
+// console.log(fizzBuzz('three'));
+
+// function fizzBuzz(input) {
+//     if ((input % 3 === 0) && (input % 5 === 0)) return 'FizzBuzz';
+//     else if (input % 3 === 0 && input % 5 !== 0) return 'Fizz';
+//     else if (input % 5 === 0 && input % 3 !== 0) return 'Buzz';
+//     else if (input !== NaN) return 'Not a number';
+//     else return input;
 // }
 
-function isLandscape(width, height) {
-    // return (width > height) ? true : false;
-    //this is considered a poor way of writing code for returning just true or false
+//Mosh answer:
+console.log(fizzBuzz(3));
+console.log(fizzBuzz(15));
+console.log(fizzBuzz(5));
+console.log(fizzBuzz(22));
+console.log(fizzBuzz('three'));
 
-    //can also use if and else:
-    // if (width > height) return true;
-    // return false;
-    //still not ideal way to write the code.
-
-    return (width > height);
-    //this will give true if width is greater than height and false if not, 
-    //much cleaner way to write the code!!
- }
-
-console.log(isLandscape(2,4));
-console.log(isLandscape(2,1));
-console.log(isLandscape(2,2));
-console.log(isLandscape(2,-1));
+function fizzBuzz(input) {
+    if (typeof input !== 'number')
+        return NaN;
+    if ((input % 3 === 0) && (input % 5 === 0)) 
+        return 'FizzBuzz';
+    if (input % 3 === 0)
+        return 'Fizz';
+    if (input % 5 === 0)
+        return 'Buzz';
+    return input;
+}
