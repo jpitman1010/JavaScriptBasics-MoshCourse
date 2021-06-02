3fcfa0f222a0aaad8c4a00d7fa834a402620ff7b
jpitman1010 <jpitman1010@gmail.com>
2021-06-01 18:28:10 -0700

    exercise -swapping variables

--- index.js
+++ index.js
@@ -1,59 +1,22 @@
-1- What is the result of “true && false”?
-a- true
-b- false   x
-c- null
-d- undefined
+//Programming Exercise, Swapping Variables
 
-2- What is the result of “(true && false) || true”?
-a- true    x
-b- false
-c- null
-d- undefined
+let a = 'red';
+let b = 'blue'; 
 
-3- What is the value of y? let x = 10; let y = (x > 10) ? 1 : 0; 
-a- 10
-b- 1
-c- 0    x
-d- 11
+//challenge, to write code to swap the two variables.
 
-4- What is the value of x? let x = (2 + 3) * (4 + 5); 
-a- 45    x
-b- 25
-c- 20
-d- 40
+// a = 'red' ? 'blue' : 'red';
+// b = 'blue' ? 'red' : 'blue';
 
-5- Which expression returns true? 
-a- ‘1’ === 1; 
-b- 1 == 1
-c- 1 === 1
-d- b and c     x
+// console.log(a);
+// console.log(b);
 
-6- What is the value of x?  let x = (1 == true); 
-a- 1
-b- true    x
-c- false 
-d- undefined 
+//success on first try!!! WOOT WOOT!!!
 
-7- What is the value of y? let x = 10;let y = (x > 5) && (x < 15); 
-a- 10
-b- 5
-c- 15 
-d- true    x
+// can also do it like this:
+let c = a;
+a = b;
+b = c;
 
-8- What is the value of x? let x = 5; x += 3; 
-a- 3
-b- 8    x
-c- 15
-d- 5 
-
-9- What is the value of y? let x = 10; let y = x++; 
-a- 10    x
-b- 11
-c- 12
-d- 13
-
-10- What is the value of y? let x = 1; let y = x !== 2;
-a- 1
-b- 2 
-c- false
-d- true    x
+console.log(a);
+console.log(b);
