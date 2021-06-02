6a7b915629051f6c7d7dd7c12338135eac9db5a6
jpitman1010 <jpitman1010@gmail.com>
2021-06-01 18:12:29 -0700

    Operators Precedence

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-//Bitwise Operators
-// Doesn't have as much practical use in most of programming
-//decimal to binary - works on the individual bits of a number
-// 1 = 00000001
-// 2 = 00000010
-// 3 = 00000011
-// R = 00000000
+//Operators Precedence
 
-console.log(1 | 2); // bitwise OR -returns 3 because it is combining 1 and 2
-console.log(1 & 2); // bitwise AND - returns 0
+// 
 
-// Read, Write, Execute
-    // 0000100  -Read
-    // 0000110 - Read and Write permissions
-    // 0000001 - Read, write and execute permissions
+let x = 2 + 3 * 4;
+// because multiplication has higher precedence it will do that operations first.
+console.log(x); //returns 14 because it does 3*4 first and then adds 2
 
-const readPermission = 4;
-const writePermission = 2;
-const executePermission = 1;
+//best to use perenthesis to have it evaluate in the order you choose.
 
-let myPermission = 0;
-myPermission = myPermission | readPermission | writePermission;
-console.log(myPermission);
-
-let message = (myPermission & readPermission) ? 'yes': 'no';
-//if this condition evaluates to true (myPermission = readPermission) it will return 'yes'
-console.log(message);  //returns yes because myPermission does have readPermission
-
-//but if you remove the readPermission from myPermission like:
-//myPermission = myPermission | writePermission;
-// you will get 'no' because the statement is no longer true.
-
-
-
-
+//but if you do:
+let y = (2 + 3) * 4;
+console.log(y); // returns 20 because it will add 2+3 first then mult by 4.
