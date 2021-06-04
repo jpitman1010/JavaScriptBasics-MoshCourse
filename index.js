94275f36c6ebabcaa0f1b54bfd40cbbd7ba8e34c
jpitman1010 <jpitman1010@gmail.com>
2021-06-04 10:45:03 -0700

    Exercise: string properties count

--- index.js
+++ index.js
@@ -1,55 +1,31 @@
-//Count Truthy
+//Exercise: String Porperties:
+//create a function called showProperties(obj) and it should
+//show all properties of the object that are type of string
 
-//crearte a function countTruthy(array) that 
-//iterates an array and counts all of the truthy elements
-//ex: 
-// const isActive = true; 
-// const name = 'Julie';
-// if (isActive) console.log('Hello'); //returns true or false
-// however:
-// if (name) console.log('Hello'); //returns truthy or falsy
-//because it is trying to evaluate a string, it returns a truthy value because
-//it is not the boolean true/false it but it is still truthy.
-//if string is empty, it is going to return falsy.
+//my answer
+const movie = {
+    title: 'a',
+    releaseYear: 2018,
+    rating: 4.5,
+    director: 'b'
+};
 
-//Falsy values are: undefined, null, '', false, 0, NaN
+showProperties(movie);
 
-//my answer:
-countTruthy([1,2,3]); //should return 3
-countTruthy([NaN,2,3]); //should return 2
-countTruthy(['',2,3]); //should return 2
-countTruthy([null,2,3]); //should return 2
-countTruthy([undefined,2,3]); //should return 2
-countTruthy([false,2,3]); //should return 2
-countTruthy([]); //should return 0
-countTruthy([NaN, undefined, 0, false, null]); //should return 0
-countTruthy([-1,2,3]); //should return 3
-
-function countTruthy(array) {
-    let truthyCount = 0
-    for (let i = 0; i <= array.length; i++) {
-        if (array[i] ? truthyCount += 1 : truthyCount += 0);
-    }
-    return console.log(truthyCount)
+function showProperties(obj) {
+    for (let key in obj) 
+        if (typeof(obj[key]) === 'string')
+            console.log(key, obj[key]);
 }
 
-//Mosh answer: 
+//Mosh Answer
 
-console.log(countTruthyMosh([1,2,3])); //should return 3
-console.log(countTruthyMosh([NaN,2,3])); //should return 2
-console.log(countTruthyMosh(['',2,3])); //should return 2
-console.log(countTruthyMosh([null,2,3])); //should return 2
-console.log(countTruthyMosh([undefined,2,3])); //should return 2
-console.log(countTruthyMosh([false,2,3])); //should return 2
-console.log(countTruthyMosh([])); //should return 0
-console.log(countTruthyMosh([NaN, undefined, 0, false, null])); //should return 0
-console.log(countTruthyMosh([-1,2,3])); //should return 3
+showPropertiesMosh(movie);
 
-
-function countTruthyMosh(array) {
-    let count = 0;
-    for (let value of array)
-        if (value)
-            count ++;
-    return count;
+function showPropertiesMosh(obj) {
+    for (let key in obj) 
+        if (typeof obj[key] === 'string')
+            console.log(key, obj[key]);
 }
+
+//difference is that I put my typeof into () and it wasn't needed
