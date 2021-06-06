97a048e95463ca60166db157d59f074adc9e2c64
jpitman1010 <jpitman1010@gmail.com>
2021-06-06 12:54:06 -0700

    constructor functions

--- index.js
+++ index.js
@@ -23,6 +23,7 @@
 //if objects have logic, we need to use factory or constructor functions.
 
 //factory function:
+//uses camel notation
 
 function createCircle(radius) {
     return {
@@ -42,3 +43,27 @@ const circle2 = createCircle(2);
 console.log(circle1);
 console.log(circle2);
 //can call circle1.draw to see 'draw' output
+
+
+
+//constructor function 
+//to construct or create an object, however naming convention is different from 
+//uses Pascal Notation: PascalNotationUsesEveryFirstLetterInUppercase
+//instead of returning like in factory function, we will use 'this' special keyword
+
+function Circle(radius) {
+    this.radius = radius;
+    //this: is a reference to the object that is executing this piece of code.
+    //objects are dynamic, and can have new properties added to them.
+    this.draw = function () {
+        console.log('draw constructor fcn circle');
+    }
+}
+const circle = new Circle(1);
+//in order to use constructor fcn, must define a constructor (in this case called circle)
+//make the const set to reserved keyword 'new' then call the fcn Circle and pass the radius
+//this creates a new javascript object and sets it equal to an empty object:
+//similar to const x = {}, next it sets the object's that have 'this' and place it into
+//the new empty object.  It then automatically returns 'this' (all things using this in object)
+
+console.log(circle)
