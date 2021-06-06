3ebc1ad8085e1eb4f4847e79c5186713e004207b
jpitman1010 <jpitman1010@gmail.com>
2021-06-06 11:15:45 -0700

    exercise stars

--- index.js
+++ index.js
@@ -1,81 +1,49 @@
-//Exercise 9 - grade 
+//show stars in rows
+//rows == number of stars in the final row
+// function is to return 1 added star (aligned left)
+//for each row up through the row parameter
 
-// take an array of grades
-// find average of array
-// if average of grades comes to:
-// 1-59 : F
-// 60-69 : D
-// 70-79 : C
-// 80-89 : B
-// 90-100 : A
+//my answer:
 
-// const marks  = [80, 80, 50]
+showStars(1);
+showStars(2);
+showStars(3);
+showStars(4);
+showStars(5);
 
-console.log(calculateGrade([100,95,98])); //outcome should be 97.6, so A should be grade
-console.log(calculateGrade([80,100,70])); //outcome should be 83.33, so B should be grade
-console.log(calculateGrade([80,80,50])); //outcome should be 70, so C should be grade
-console.log(calculateGrade([20,70,100])); //outcome should be 63.3, so D should be grade
-console.log(calculateGrade([0,0,0])); //outcome should be 0, so F should be grade
+function showStars(rows) {
+    let stars = makeStarArray(rows);
+    for (star of stars)
+        console.log(star);
+}
 
-//my answer: 
-
-function calculateGrade(marks) {
-    let grade = 0;
-
-    for (let mark of marks) grade += mark; 
-
-    grade /= marks.length;
-    
-    if (grade >= 90 && grade <= 100) return "A";
-    else if (grade >= 80 && grade < 90) return "B";
-    else if (grade >= 70 && grade < 80) return "C";
-    else if (grade > 59 && grade < 70) return "D";
-    else return "F";
+function makeStarArray(rows) {
+    let starsArray = ['*'];
+    if (rows >= 2) {
+        for (let i = 1; i < rows; i++) {
+            starsArray[i] = starsArray[i-1] + '*';
+        }
+    }
+    return starsArray;
 }
 
 
-console.log(calculateGradeMosh([100,95,98])); //outcome should be 97.6, so A should be grade
-console.log(calculateGradeMosh([80,100,70])); //outcome should be 83.33, so B should be grade
-console.log(calculateGradeMosh([80,80,50])); //outcome should be 70, so C should be grade
-console.log(calculateGradeMosh([20,70,100])); //outcome should be 63.3, so D should be grade
-console.log(calculateGradeMosh([0,0,0])); //outcome should be 0, so F should be grade
 
+//Mosh answer:
 
-function calculateGradeMosh(marks) {
-    let sum = 0;
-    for (let mark of marks)
-        sum += mark;
-    let average = sum/ marks.length;
+showStarsMosh(0);
+showStarsMosh(1);
+showStarsMosh(2);
+showStarsMosh(3);
+showStarsMosh(4);
+showStarsMosh(5);
 
-    if (average < 60) return 'F';
-    if (average < 70) return 'D';
-    if (average < 80) return 'C';
-    if (average < 90) return 'B';  
-    return 'A';
+function showStarsMosh(rows) {
+    for (let row = 1; row <= rows; row++) {
+        let pattern = '';
+        for (let i = 0; i < row; i++)
+            pattern += '*';
+        console.log(pattern);
+    }
 }
-
-//even better, extract some of the work into a separate function:
-console.log(calculateAverageMosh2([100,95,98])); //outcome should be 97.6, so A should be grade
-console.log(calculateAverageMosh2([80,100,70])); //outcome should be 83.33, so B should be grade
-console.log(calculateAverageMosh2([80,80,50])); //outcome should be 70, so C should be grade
-console.log(calculateAverageMosh2([20,70,100])); //outcome should be 63.3, so D should be grade
-console.log(calculateAverageMosh2([0,0,0])); //outcome should be 0, so F should be grade
-
-
-function calculateAverage(array) {
-    let sum = 0;
-    for (let num of array)
-        sum += num;
-    return sum/ array.length;
-}
-
-function calculateAverageMosh2(array) {
-    let average = calculateAverage(array);
-    if (average < 60) return 'F';
-    if (average < 70) return 'D';
-    if (average < 80) return 'C';
-    if (average < 90) return 'B';  
-    return 'A';
-}
-
 
