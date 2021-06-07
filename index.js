38743fb1b9f3d9f7800fab139a82992f5dc33995
jpitman1010 <jpitman1010@gmail.com>
2021-06-07 12:01:51 -0700

    exercise equality of objects and properties

--- index.js
+++ index.js
@@ -1,61 +1,12 @@
-// exercise 2, Address factory and constructor functions
 
-//create address object, write 2 diff fcns (factory and constructor) 
-//to initialize an address object
+//Equality
 
-//my answer:
+//use constructor function and compare 2 addresses to see that each of their properties
+//are equal to one another.  === will check for same point in memory, so can't use that
+//and will need to check each property.
+// second function should be written to see if the objects are the same, are they referencing
+//the same objects.
 
-//factory function:  
-
-function showAddress(street, city, zipCode) {
-    return {
-        street,
-        city,
-        zipCode
-    }
-}
-
-const address1 = showAddress('123 Fake Street', 'Springfield', '11111');
-
-const address2 = showAddress('123 Simpsons Street', 'Springflied2', '22222');
-
-console.log(address1);
-
-console.log(address2);
-
-
-//constructor function:
-
-function ShowAddress(street, city, zipCode) {
-    this.street = street;
-    this.city = city;
-    this.zipCode = zipCode
-}
-
-const address3 = new ShowAddress('987 Fake Street', 'FakeCity', '33333');
-
-const address4 = new ShowAddress('654 AnotherFakeStreet', 'AnotherFakeCity', '44444');
-
-console.log(address3);
-
-console.log(address4);
-
-
-//Mosh answer:
-
-//factory function 
-
-function createAddress(street, city, zipCode) {
-    return {
-        street,
-        city,
-        zipCode
-    };
-}
-
-let address5 = createAddress('a', 'b', 'c');
-
-console.log(address5);
 
 //constructor function:
 
@@ -65,6 +16,51 @@ function Address(street, city, zipCode) {
     this.zipCode = zipCode;
 }
 
-let address6 = new Address('d', 'e', 'f');
+let address1 = new Address('d', 'e', 'f');
+let address2 = new Address('d', 'e', 'f');
 
-console.log(address6)
+//my answer: 
+
+function areEqual(address1, address2) {
+    for (key in address1) {
+        for (key in address2){
+            if (address1[key] != address2[key]) return false;
+        }
+    }
+    return true;
+}
+
+console.log(areEqual(address1, address2));
+
+function areSame(address1, address2) {
+    if (address1 !== address2) return false;
+}
+
+console.log(areSame(address1, address2));
+
+
+
+
+//Mosh answer: 
+function areEqualMosh(address1, address2) {
+    return address1.street === address2.street &&
+    address1.city === address2.city && 
+    address1.zipCode === address2.zipCode;
+}
+
+console.log(areEqualMosh(address1, address2))
+
+function areSameMosh(address1, address2) {
+    return address1 === address2;
+}
+
+console.log(areSameMosh(address1, address2));
+
+
+//if we set a new variable of address3 = address1 and check for if they are the same,
+//they will be (not part of exercise, just good to know):
+
+let address3 = address1;
+
+console.log(areSameMosh(address1, address3)); //outcome is now true because they
+//are pointing to the same place in memory.
