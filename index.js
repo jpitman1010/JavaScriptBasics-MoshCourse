d2ffb01a7e7ca0abb1e605db4a1d3a12560be8c4
jpitman1010 <jpitman1010@gmail.com>
2021-06-06 21:20:20 -0700

    Value and Reference Types

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
-// functions are objects:
+// Value types are primitives and are copied by their values
+// object types are references and are copied by their reference
 
-//if you want to pass an object as a parameter you can use ` to go onto multiple lines:
+let x = 10;
+let y = x;
 
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
+x = 20;
+
+
+console.log(x); //output is 20
+console.log(y); //output is 10
+
+//x and y are independent of each other.  
+
+//so if you change the variable of x to an object (we will call it xx)
+
+let xx = { value: 20};
+let yy = xx;
+
+console.log(xx); //output {value: 20}
+console.log(xx.value); //output
+console.log(yy); 
+
+//when we use an object it is not stored in the variable, it is stored in an external 
+//place in memory.  Both xx and yy are pointing to the same place in the memory.
+
+//primitives are copied by values, reference types are copied by variables.
+
+let number = 10;
+
+function increase(number) {
+    number++;
 }
 
-const Circle2 = new Function('radius', `
-this.radius = radius;
-this.draw = function() {
-    console.log('draw')
+increase(number);
+console.log(number); //output is 10 because the value is copied into the parameter 
+//of the function, so the value assigned to number is only going to change the parameter
+//within the function.
+
+//if we change number to an object, it will be able to point to the same point in memory
+// and give the desired output.
+
+let obj = { value: 10};
+
+function increaseObj(obj) {
+    obj.value++;
 }
-`);
-
-const anotherCircle = new Circle(1);
-const anotherCircle2 = new Circle2(2);
-
-console.log(anotherCircle);
-console.log(anotherCircle2);
-
-//methods available
-
-Circle.call({}, 1);
-//the first argument of the call method specifies the target: 'this' 
-console.log('logging call method', Circle.call({}, 1));
-
-
-Circle.apply({}, [1]);
-//the first argument of the call method specifies the target: 'this' 
-console.log('logging apply method', Circle.apply( {}, [1]));
+increaseObj(obj);
+console.log(obj); //output of object's value is now 11 because obj is passed by it's
+//reference.  
