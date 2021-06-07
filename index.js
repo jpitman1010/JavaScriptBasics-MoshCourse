0361e9b35047cd898d018bcf5488a7e9c31c5bef
jpitman1010 <jpitman1010@gmail.com>
2021-06-07 15:35:40 -0700

    testing the elements of an Array

--- index.js
+++ index.js
@@ -1,68 +1,22 @@
-//Arrays - sorting
+//Testing the elements of an array
 
-const numbers = [2, 3, 4, 1];
+const numbers = [1, -1, 2, 3];
 
-//when you have numbers and strings in an array, this works:
-numbers.sort();
-
-console.log(numbers);
-
-numbers.reverse();
-
-console.log(numbers);
-
-
-//but when you have objects in an array:
-
-const courses = [
-    { id: 1, name: 'Node.js'},
-    { id: 2, name: 'JavaScript'}
-];
-
-//say you wanted to sort by name;
-
-//the sort method takes an argument, so we can pass a function to compare the objects
-
-courses.sort(function(a, b) {
-    if (a.name < b.name) return -1;
-    if (a.name > b.name) return 1;
-    return 0;
+const allPositive = numbers.every(function(value) {
+    return value >= 0;
 });
 
-console.log(courses);
+console.log(allPositive); //as soon as you hit the first negative number the 
+//method terminates
 
-//however if Javascript is changed to javascript, due to the ASCII numbers assigned
-//to characters, the uppercase will come before the lowercase.
+//if we replace every with some, and save changes it will show 
 
 
-const courses2 = [
-    { id: 1, name: 'Node.js'},
-    { id: 2, name: 'javaScript'}
-];
-
-courses2.sort(function(a, b) {
-    if (a.name < b.name) return -1;
-    if (a.name > b.name) return 1;
-    return 0;
+const oneOrMorePositives = numbers.some(function(value) {
+    return value >= 0;
 });
 
-console.log(courses2);
-
-//to fix this issue of uppercase and lowercase:
+console.log(oneOrMorePositives); //returns true because there is at least 1 positive
 
 
-const courses3 = [
-    { id: 1, name: 'Node.js'},
-    { id: 2, name: 'javaScript'}
-];
-
-courses3.sort(function(a, b) {
-    const nameA = a.name.toUpperCase();
-    //can also use toLowerCase
-    const nameB = b.name.toUpperCase();
-    if (nameA < nameB) return -1;
-    if (nameA > nameB) return 1;
-    return 0;
-});
-
-console.log(courses3);
+//some older browsers don't support this.
