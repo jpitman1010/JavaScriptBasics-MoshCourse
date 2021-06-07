c4d8dc539ad10c8ef4d40e993bc9f3aeb7112cf6
jpitman1010 <jpitman1010@gmail.com>
2021-06-07 15:19:54 -0700

    Arrays - joining (and splicing strings)

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-//Iterating Array
+//Arrays - joining
 
 const numbers = [1, 2, 3, 4];
 
-for (let number of numbers)
-    console.log(number);
+const joined = numbers.join(','); //join method returns a string, 
+//notice when you type join( it gives the pop up that shows what you can use
+//as a parameter, anytime it has a ? it means it is an optional parameter, so you can
+//or don't have to use it.
 
-//this works the same:
-numbers.forEach(function(number){
-    console.log(number);
-});
+console.log(joined);
 
-//this also works the same:
 
-numbers.forEach(number => 
-    console.log(number)
-);
+//splice is often used with join, but it only works on strings, not on arrays.
 
-//can also add a second parameter:
+const message = 'This is my message.';
+const parts = message.split(' ');
 
-numbers.forEach((number, index) => console.log(index, number));
+console.log(parts);
+
+const combined = parts.join('-');
+
+console.log(combined);
+
+//this is used for example if you got to stack overflow for an issue, the title will be 
+//separated with white spaces like you would expect when you are reading, but if 
+//you look at url for that page, the title is in the url, and you cannot use white
+//spaces in url.  the words of title in url are combined/joined using the hyphen.
