c8fd3a781d74d7f383b105e7b39c60e91edc9956
jpitman1010 <jpitman1010@gmail.com>
2021-06-07 13:23:05 -0700

    post constructor function

--- index.js
+++ index.js
@@ -1,58 +1,49 @@
-//exercise Blog Post Object
+//exercise constructor functions
 
-//create an object literal syntax to create
-// blog post object that has following properties:
-
-    //title
-    //body
-    //author
-    //views
-    //comments
-        //comments should have properties:
-        //author
-        //body
-    //isLive
-        //should have properties:
-        //true
-        //false
-
-
-//my answer: 
-function BlogPost() {
-    for (key in blogPostObj) {
-        console.log(blogPostObj[key])
-    }
-}
-
-const blogPostObj = {
-    title: 'Best Title Ever!', 
-    body: 'This is about something.',
-    author: 'Anonymous',
-    views: 22,
-    comments: {
-        author: 'No Name Nancy',
-        body: `I don't like that!`
-    },
-    isLive: true
-    };
-
-console.log(blogPostObj);
-
-BlogPost();
-
-//not sure I understood the exercise completely:
-
-//Mosh answer:
-
-let post  = {
+let post = {
     title: 'a',
     body: 'b',
     author: 'c',
+    views: 10,
     comments: [
-        {author: 'a', body: 'b'},
-        {author: 'c', body: 'd'}
+        {author: 'author1', body: 'body1'},
+        {author: 'author2', body: 'body2'}
     ],
     isLive: true
 };
 
-console.log(post);
+//create a constructor function to create a post object
+//user is drafting a post but it has not been published yet.
+
+
+//my answer:
+function CreatePost(title, body, author, views, comments, isLive) {
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = views;
+    this.comments = comments;
+    this.isLive = isLive;
+}
+
+const post1 = new CreatePost('a', 'b', 'c', 0, ['author1', 'body1'], false);
+console.log(post1);
+
+
+//Mosh answer:
+
+function Post(title, body, author) {
+    //don't use views as parameter cause that is automatically zero for creation of post
+    //don't use comments as parameter because it is not posted so cannot have comments
+    //don't use isLive as parameter because it will be set to false initially.
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = 0;
+    this.comments = [];
+    this.isLive = false;
+}
+
+let postMosh = new Post('a','b','c');
+
+console.log(postMosh);
