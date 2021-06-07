c85ca11d4e3ddf90c9f13eb501e2289046caf862
jpitman1010 <jpitman1010@gmail.com>
2021-06-07 10:51:51 -0700

    template literals

--- index.js
+++ index.js
@@ -1,69 +1,48 @@
-//built in string object
+//template literals: 
 
-const message = 'This is my first message';
+//Literals are used for:
+    //Object === {}
+    //Boolean === true, false
+    //String === '',""
+    //Template === ``
 
-//why when we use . after a string we have declaired, do we get a bunch of method 
-//options when string is a primitive object and cannot have methods??
+//Template literals help us to write cleaner code.
 
-//because there are two types of strings, you can have the string primitive object 
-//like above, then you can also make a String('hi') object that allows you to use
-//functions built into Javascript for the string object.
+const message = `This is my
+first message`; //by putting the back tics and hitting enter where you 
+//want the line break, it will create it without the string literal \n
 
-const anotherString = new String('hi');
-console.log('this is the typeof the message = ', typeof(message))
-console.log('this is the typeof the anotherString object = ', typeof(anotherString))
+const message2 = 
+`This is my
+first message`;
 
-
-//however, if you try to use the dot methods with the string, JavaScript in the background
-//will wrap the string behind the scenes into a string object for us, allowing us to use
-//the methods (which is why you can see a list of methods when you type message. )
-
-//learn more in the docs, google javascript string, go to Mozilla docs
-
-console.log(message.length);
-console.log(message[1]);
-console.log(message.includes('my'));//returns true or false, the searchers are case
-//sensitive.
-console.log(message.endsWith('e')); //can find if anything ends with the letter 'e'
-console.log(message.indexOf('my')); //it will give 8 because in message the word my
-//starts at index 8
-console.log(message.replace('first', 'second')); //this creates a new string and does not
-//modify the original string...
 console.log(message);
+console.log(message2); //they come out exactly the same.
 
-console.log(message.toUpperCase()); //also returns a new string, doesn't modify original string
+//great for using to write emails, allows you to see the formatting:
+
+const message3 = 
+`Hi Mosh,
+
+Thank you for teaching this class!!
+
+Regards,
+Julie`;
+
+console.log(message3);  //comes out exactly as written!!
+
+//can add placeholders too!!  
 
 
-const message2 = '   I am going to  use this    message to show the trim    method.      ';
-console.log(message2);
-console.log(message2.trimRight());
-console.log(message2.trimLeft());
-console.log(message2.trimStart());
-console.log(message2.trimEnd());
-//notice it doesn't trim mid parts of the string.
+const somebody = 'Mosh';
+const me = 'Julie';
 
+const message4 = 
+`Hi ${somebody},
 
-//if you wish to use a single quote for an apostrophe inside your string, 
-//but you have used single quotes around your string, you can use the ESCAPE
-// key  \   to indicate to not treat it as the end of the string.
+Thanks for teaching this course!
 
-const message3 = 'I can\'t, won\'t, shouldn\'t, but I will!';
-console.log(message3);
+Regards,
+${me}`;
 
-//   \n will create a new line
-
-const message4 = 'I \nwant\nevery\nword\non\na\nnew\nline.';
 console.log(message4);
-
-const message5 = 'Using split method for the string object.';
-console.log(message5.split(' ')); //output is an array of each word split based on
-//the ' '(space) or any other character chosen to split by.
-
-
-
-
-
-
-
-
-
