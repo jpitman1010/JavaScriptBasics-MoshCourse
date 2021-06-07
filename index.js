a26d22bedb23757304bb9c9b1fb6af46be71108d
jpitman1010 <jpitman1010@gmail.com>
2021-06-07 10:10:21 -0700

    cloning object

--- index.js
+++ index.js
@@ -1,50 +1,32 @@
-// Value types are primitives and are copied by their values
-// object types are references and are copied by their reference
+//cloning an object
 
-let x = 10;
-let y = x;
+//this is older approach...
+const circle = {
+    radius: 1,
+    draw() {
+        console.log('draw')
+    }
+};
 
-x = 20;
+// const another = {};
 
+// for (let key in circle)
+//     another[key] = circle[key];
 
-console.log(x); //output is 20
-console.log(y); //output is 10
+// console.log(another);
 
-//x and y are independent of each other.  
+//this is a better/ newer approach
 
-//so if you change the variable of x to an object (we will call it xx)
+// const another = Object.assign({
+//     color: 'yellow'
+//     //you can assign new key value pairs using this method.
+// }, circle);
 
-let xx = { value: 20};
-let yy = xx;
+// console.log(another);
 
-console.log(xx); //output {value: 20}
-console.log(xx.value); //output
-console.log(yy); 
+//even more simple and elegant way to clone an object is: 
 
-//when we use an object it is not stored in the variable, it is stored in an external 
-//place in memory.  Both xx and yy are pointing to the same place in the memory.
-
-//primitives are copied by values, reference types are copied by variables.
-
-let number = 10;
-
-function increase(number) {
-    number++;
-}
-
-increase(number);
-console.log(number); //output is 10 because the value is copied into the parameter 
-//of the function, so the value assigned to number is only going to change the parameter
-//within the function.
-
-//if we change number to an object, it will be able to point to the same point in memory
-// and give the desired output.
-
-let obj = { value: 10};
-
-function increaseObj(obj) {
-    obj.value++;
-}
-increaseObj(obj);
-console.log(obj); //output of object's value is now 11 because obj is passed by it's
-//reference.  
+const another = { ...circle };
+//this ... spreads all properties and methods of the object and spreads it into
+//the object (brackets) to which you are putting into the newly cloned object.
+console.log(another);
