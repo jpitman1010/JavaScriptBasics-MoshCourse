7d5ee0be617cab124ecb81d468e739cbc0ef6441
jpitman1010 <jpitman1010@gmail.com>
2021-06-07 15:05:03 -0700

    Arrays- Combining and Slicing

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
-//Arrays -removing elements
+//Arrays - combinging and slicing arrays
 
-const numbers = [1, 2, 3, 4];
-console.log(numbers);
+const first  = [1, 2, 3];
+const second = [4, 5, 6];
+
+//to combine use combine
+
+//concat method, but it will create a new array to combine the two,
+
+const combined = first.concat(second);
+
+console.log(combined);
+
+//if you want to get parts of an array, use slice: 
+
+const sliceCombined = combined.slice(2,4);
+
+console.log(sliceCombined);
+
+const combined2 = first.concat(second);
+
+console.log(combined2);
+
+const sliceGetAll = combined2.slice();
+
+console.log(sliceGetAll);
+
+const sliceGetFrom2ndIndex = combined2.slice(2);
+
+console.log(sliceGetFrom2ndIndex);
 
 
-//removing from end:
-const last  = numbers.pop();
-console.log(numbers);
-console.log(last);
+//when combining arrays, if you substitute in an array with an
+// object with key and value pair
+// when you copy the array, it copies the object's reference, not the value itself
+//so if you were to set firstObjArray and then change the value, it will change in 
+//console.log:
 
+const firstObjArray = [{ id: 1 }];
 
-//removing from beginning:
-const first = numbers.shift();
-console.log(numbers);
-console.log(first);
+const combined3 = firstObjArray.concat(second);
 
-//removing from middle:
-//get back to original array by adding the numbers back:
-numbers.unshift(1);
-numbers.push(4);
-console.log(numbers);
+console.log(combined3);
 
-const middle = numbers.splice(2,1)
-console.log(numbers); //3 has been removed because it is in index 2
-console.log(middle);
+firstObjArray[0].id = 10; 
+
+console.log(combined3); // now the first object shows referenced value for id as 10
+
+//same principle applies to splice method.
