22b83d81e7e9b488f43803d3ac815e23f102eb03
jpitman1010 <jpitman1010@gmail.com>
2021-06-08 22:56:13 -0700

    Function Declaration vs Epression

--- index.js
+++ index.js
@@ -1,47 +1,25 @@
-//exercise - Movies
+// Function Declaration
 
-const movies = [
-    { title: 'a', year: 2018, rating: 4.5 },
-    { title: 'b', year: 2018, rating: 4.7 },
-    { title: 'c', year: 2018, rating: 3 },
-    { title: 'd', year: 2017, rating: 4.5 },
-];
-
-//all the movies from 2018 with rating > 4;
-//sort by rating in descending order (higher score first)
-//show only their title:
-//result should be: 
-// 'b'
-// 'a'
-
-
-function movieRating(movies) {
-    let movieList = []
-    for (movie of movies) {
-        if (movie.year === 2018 && movie.rating >= 4)
-            movieList.push(movie.title);
-    }
-    
-    movieList.sort(function(current, next) {
-        if (current > next) 
-            console.log(current);
-            console.log(next);
-    })
+function walk() {
+    console.log('walk');
 }
 
+//Anonymous Function Expression
 
-movieRating(movies);
+let run = function () {
+    console.log('run');
+};
+//anytinme we declair a variable like run, even if
+//it is set to a function, we complete it with
+// a semicolon
 
+// Named Function Expression
 
-//Mosh answer 
+let runFast = function fast() {
+    console.log('run fast')
+};
 
-const titles = movies 
-    .filter(m => m.year === 2018 && m.rating >= 4)
-    .sort((a,b) => a.rating - b.rating)
-    .reverse()
-    .map(m => m.title);
-
-console.log('Mosh\'s answer', titles);
-        
-        
+run();
+let move = run;
+move();
 
