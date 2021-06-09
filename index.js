d5088d0174312938a5be46b700b1d54f95e7891a
jpitman1010 <jpitman1010@gmail.com>
2021-06-08 23:01:11 -0700

    Hoisting

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 // Function Declaration
 
+//with function declarations, we can call the function
+//before we write the function:
+walk();
+//in JavaScript, a Function Declaration will move
+//all functions to the top before running them
+//which is called Hoisting (the process of moving
+// function declarations to the top of the file done 
+//automatically by JavaScript engine running the code.)
+
 function walk() {
     console.log('walk');
 }
 
 //Anonymous Function Expression
+    //cannot call function before it has been
+    //declaired.
+    //if you try to call run() prior to it being
+    //declaired, you get the following error:
+    // Uncaught ReferenceError: run is not defined
+    //same thing happens when you do 
+    // console.log(x);
+    // let x = 1;
 
 let run = function () {
     console.log('run');
@@ -22,4 +39,6 @@ let runFast = function fast() {
 run();
 let move = run;
 move();
+
+
 
