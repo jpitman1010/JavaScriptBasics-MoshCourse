5a555efa64d390861019c4912b0667edc2ca3add
jpitman1010 <jpitman1010@gmail.com>
2021-06-08 18:04:02 -0700

    exercise Moving parts of array

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-//Exercise  - except:
-//build a function that passes 2 parameters, numbers array and excluded.  if 
-//excluded is a number in the array, return a new array exlcluding that number.
+//exercise -Moving an element
 
-const numbers = [1, 2, 3, 4, 1 , 2, 1];
+ const numbers = [1, 2, 3, 4];
+//Mosh Answer
 
-const output = except(numbers, [1,2]);
-
+const output = moveMosh(numbers, 0, 0); //returns original array
 console.log(output);
+console.log(moveMosh(numbers, 3, 0)); //returns original array
+console.log(moveMosh(numbers, 0, 1)); // passing 1 as offset, 1&2 swap places
+// console.log(moveMosh(numbers, 4, 1)); // passing -2 or less as offset from index 1
+// //, output = console.error('Invalid offset');
+// console.log(moveMosh(numbers, 0, 5)); // passing 4 or more as offset, output =
+// //console.error('Invalid offset');
+// console.log(moveMosh(numbers, 3, 2)); // passing -2 or less as offset from index 1
+// //, output = console.error('Invalid offset');
+// console.log(moveMosh(numbers, 0, -1));  // passing -1 or less as offset from index 0
+// //, output = console.error('Invalid offset');
+// console.log(moveMosh(numbers, 1, -2)); // passing -2 or less as offset from index 1
+// //, output = console.error('Invalid offset');
+console.log(moveMosh(numbers, 0, 2)); // passing 2 as offset, output = [2, 3, 1, 4];
+console.log(moveMosh(numbers, 0, 3)); // passing 3 as offset, output = [2, 3, 4, 1];
+console.log(moveMosh(numbers, 1, -1)); // passing -1 as offset at index 1, output = [2, 1, 3, 4];
+console.log(moveMosh(numbers, 1, 2)); // passing 2 as offset, output = [1, 3, 4, 2];
+console.log(moveMosh(numbers, 3, -2)); // passing -1 as offset at index 1, output = [1, 4, 2, 3];
 
-//my answer:
 
-function except(array, excluded) {
-    const newArray = array
-    for (let number of excluded) 
-        for (let num of array) 
-            if (num === number) {                
-                const findIndexToRemove = newArray.indexOf(num);
-                newArray.splice(findIndexToRemove,findIndexToRemove+1); 
-            }
-    return newArray
-}
+function moveMosh(array, index, offset) {
+    const position = index + offset; 
+    if (position >= array.length || position < 0) {
+        console.error('Invalid Offset.');
+        return;
+    }
 
-
-//Mosh answer: 
-
-const outputMosh = exceptMosh(numbers, [1,2]);
-
-console.log(outputMosh);
-
-function exceptMosh(array, excluded) {
-    const output = [];
-    for (let element of array)
-        if (!excluded.includes(element))
-            output.push(element);
+    const output = [...array];
+    const element = output.splice(index, 1)[0];
+    output.splice(position, 0, element);
     return output;
 }
