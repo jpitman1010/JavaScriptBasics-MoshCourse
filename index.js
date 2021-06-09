a44ce3acfdb4d41ca30030f92ef9bea15073c3ab
jpitman1010 <jpitman1010@gmail.com>
2021-06-08 23:09:43 -0700

    arguments

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
-// Function Declaration
+//arguements
 
-//with function declarations, we can call the function
-//before we write the function:
-walk();
-//in JavaScript, a Function Declaration will move
-//all functions to the top before running them
-//which is called Hoisting (the process of moving
-// function declarations to the top of the file done 
-//automatically by JavaScript engine running the code.)
-
-function walk() {
-    console.log('walk');
+function sum(a, b) {
+    return a + b;
 }
 
-//Anonymous Function Expression
-    //cannot call function before it has been
-    //declaired.
-    //if you try to call run() prior to it being
-    //declaired, you get the following error:
-    // Uncaught ReferenceError: run is not defined
-    //same thing happens when you do 
-    // console.log(x);
-    // let x = 1;
+console.log(sum(1, 2));
+//you can also pass only 1 argument
+console.log(sum(1));
+//result is NaN because the sum of 1 + undefined == NaN
 
-let run = function () {
-    console.log('run');
-};
-//anytinme we declair a variable like run, even if
-//it is set to a function, we complete it with
-// a semicolon
-
-// Named Function Expression
-
-let runFast = function fast() {
-    console.log('run fast')
-};
-
-run();
-let move = run;
-move();
+//same happens if we pass no arguments or additional arguments:
+console.log(sum()); //results in NaN
+console.log(sum(1, 2, 3, 4, 5)); //result is still 3, 
+//because only the first 2 arguments are being used.
 
 
+//if you want a varied number of parameters in a function
+//every function has a built in function called
+// arguments (sort of looks like array but it is
+//actually an object, they keys being the indexes 
+//to the arguments passed in the function)
 
+function sum2(a, b) {
+    console.log(arguments);
+    return a + b;
+}
+
+console.log(sum2(1, 2, 3, 4, 5)); 
+
+//to change the function to add all the arguments passed
+//into  the function:
+
+function sum3() {
+    let total = 0;
+
+    for (let value of arguments)
+        total += value;
+    return total
+}
+
+console.log(sum2(1, 2, 3, 4, 5)); 
